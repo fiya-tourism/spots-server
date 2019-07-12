05a3456cc9c10be64972ffed5b57ad8faea46e06
Your Name <email@example.com>
2019-07-12 22:52:03 +0800

    提交基础项目

---
+++ spots-server/src/main/java/com/fy/spotsserver/SpotsServerApplication.java
@@ -0,0 +1,13 @@
+package com.fy.spotsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotsServerApplication.class, args);
+    }
+
+}
